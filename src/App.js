b1c1dbb2e25633b79fec4fa7f1908843e4b426cc
Philippe Victoria <vp8@mbp-de-philippe.home>
2023-02-04 18:53:20 +0100

    problème avec la checkbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import logo from "../src/assets/img/logo.png";
 
-function App() {
+const App = () => {
+  const [task, setTask] = useState("");
+  const [tab, setTab] = useState([]);
+  const [check, setCheck] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src={logo} alt="" />
+        <h1>To Do List</h1>
       </header>
+
+      <div className="tasktodisplay">
+        {tab.map((elem) => {
+          return (
+            <div className="tasktodisplay2">
+              <input
+                className="checkbox"
+                type="checkbox"
+                onClick={() => {
+                  {
+                    check === false ? setCheck(true) : setCheck(false);
+                  }
+                }}
+              />
+              <p className={check === true ? "check" : null}>{elem}</p>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="container">
+        <input
+          className="tasktoadd"
+          placeholder="New task"
+          type="text"
+          name="task"
+          value={task}
+          onChange={(event) => {
+            setTask(event.target.value);
+          }}
+        />
+
+        <button
+          className="addtask"
+          onClick={() => {
+            const newTab = [...tab];
+            newTab.push(task);
+            setTab(newTab);
+            console.log(tab);
+          }}
+        >
+          Add task
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
